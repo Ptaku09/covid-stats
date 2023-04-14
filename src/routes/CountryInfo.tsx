5ff39f7add555b7b2222c9991b241c142d69ccc0
Mateusz Ptak <ptakm02@gmail.com>
2023-04-14 15:14:52 +0200

    country-page: move world map logic to separate component

--- src/routes/CountryInfo.tsx
+++ src/routes/CountryInfo.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import withRouter, { WithRouterProps } from '../utils/withRouter';
-import { ComposableMap, Geographies, Geography, Graticule, Sphere } from 'react-simple-maps';
 import memoFetch from '../utils/memoFetch';
 import { CountryDailyInfo } from '../types/country';
+import WorldMap from '../components/organisms/WorldMap';
 
 interface MyState {
   data: CountryDailyInfo[] | null;
@@ -35,47 +35,11 @@ class CountryInfo extends Component<WithRouterProps, MyState> {
   render() {
     const { data, isError } = this.state;
 
+    console.log(data);
+
     return (
       <div className="w-full h-full flex items-center justify-center">
-        {data ? (
-          <div className="hidden md:flex w-5/6 px-5 border-2 border-neutral-200">
-            <ComposableMap
-              projectionConfig={{
-                rotate: [-10, 0, 0],
-                scale: 147,
-              }}
-            >
-              <Sphere stroke="#E4E5E6" strokeWidth={0.5} fill="transparent" id="test" />
-              <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-              <Geographies geography="/custom-light.geo.json">
-                {({ geographies }) =>
-                  geographies.map((geo) => {
-                    const { name_en: name } = geo.properties;
-                    const { Country: countryName } = data[0];
-
-                    return (
-                      <Geography
-                        key={geo.rsmKey}
-                        geography={geo}
-                        fill={countryName.startsWith(name) ? 'rgb(0 123 255)' : '#ccc'}
-                        stroke="#fff"
-                        style={{
-                          default: { outline: 'none' },
-                          hover: { fill: 'rgb(191 219 254)', outline: 'none' },
-                          pressed: { outline: 'none' },
-                        }}
-                      />
-                    );
-                  })
-                }
-              </Geographies>
-            </ComposableMap>
-          </div>
-        ) : isError ? (
-          <div>Something went wrong</div>
-        ) : (
-          <div>Loading...</div>
-        )}
+        {data ? <WorldMap countryName={data[0].Country} /> : isError ? <div>Something went wrong</div> : <div>Loading...</div>}
       </div>
     );
   }
