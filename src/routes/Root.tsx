b8c9945b825e30cb8926e7a1f2803f9d8a127365
Mateusz Ptak <ptakm02@gmail.com>
2023-04-15 21:21:29 +0200

    feat: merge Summary template with Root route

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
-import Summary from '../components/templates/Summary';
+import memoFetch from '../utils/memoFetch';
+import { TotalSummary } from '../types/summary';
+import GlobalDataSummary from '../components/molecules/GlobalDataSummary';
+import SummaryTable from '../components/organisms/SummaryTable';
+import TooManyRequestsError from '../components/atoms/TooManyRequestsError';
+import GlobalDataSummaryLoader from '../components/molecules/GlobalDataSummaryLoader';
+import SummaryTableLoader from '../components/molecules/SummaryTableLoader';
 
-class Root extends Component<{}, {}> {
+interface MyState {
+  data: TotalSummary | null;
+  isError: boolean;
+}
+
+class Root extends Component<{}, MyState> {
+  constructor(props = {}) {
+    super(props);
+    this.state = {
+      data: null,
+      isError: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchSummaryData();
+  }
+
+  fetchSummaryData() {
+    memoFetch(`${process.env.REACT_APP_BASE_URL}/summary`)
+      .then((data) => this.setState({ data }))
+      .catch((_) => this.setState({ isError: true }));
+  }
+
   render() {
+    const { data, isError } = this.state;
+
     return (
-      <div className="w-screen h-auto">
-        <Summary />
+      <div className="w-screen h-auto flex justify-start items-center flex-col gap-10">
+        {data ? (
+          <>
+            <GlobalDataSummary data={data} />
+            <SummaryTable data={data} />
+          </>
+        ) : isError ? (
+          <TooManyRequestsError />
+        ) : (
+          <>
+            <GlobalDataSummaryLoader />
+            <SummaryTableLoader />
+          </>
+        )}
       </div>
     );
   }
