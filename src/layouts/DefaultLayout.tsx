005fa76d0db9d6b6e17d7f82663cf21c22fd0606
Mateusz Ptak <ptakm02@gmail.com>
2023-04-15 23:16:03 +0200

    react18: convert layouts to functional components

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import Header from '../components/molecules/Header';
 import Footer from '../components/molecules/Footer';
 
@@ -6,20 +6,14 @@ interface MyProps {
   children: ReactNode;
 }
 
-class DefaultLayout extends Component<MyProps, {}> {
-  constructor(props: MyProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="relative bg-white min-h-screen">
-        <Header />
-        <main className="w-full h-auto py-12 pb-24 lg:p-0 lg:pb-4 lg:-translate-y-20 font-rubik">{this.props.children}</main>
-        <Footer />
-      </div>
-    );
-  }
-}
+const DefaultLayout = ({ children }: MyProps) => {
+  return (
+    <div className="relative bg-white min-h-screen">
+      <Header />
+      <main className="w-full h-auto py-12 pb-24 lg:p-0 lg:pb-4 lg:-translate-y-20 font-rubik">{children}</main>
+      <Footer />
+    </div>
+  );
+};
 
 export default DefaultLayout;
